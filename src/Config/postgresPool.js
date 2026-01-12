5fae23610c813222ec00f20e1547970f2f32ec91
Eduardo Garro <efgarro@yahoo.com>
2026-01-11 21:22:30 -0600

    update config files

--- src/Config/postgresPool.js
+++ src/Config/postgresPool.js
@@ -1,10 +1,10 @@
-// import { join } from "node:path";
-// import dotenv from "dotenv";
-// dotenv.config({
-//   path: join(import.meta.dirname, "../../.env"),
-// });
+import { join } from "node:path";
+import dotenv from "dotenv";
+dotenv.config({
+  path: join(import.meta.dirname, "../../.env"),
+});
 
-import { secrets } from "../Utils/aws-secrets.js";
+// import { secrets } from "../Utils/aws-secrets.js";
 
 // // Your code goes here
 // console.log("secret");
@@ -16,36 +16,36 @@ import { secrets } from "../Utils/aws-secrets.js";
 import pg from "pg";
 const { Pool } = pg;
 
-const scrPool = new Pool({
-  host: "54.205.165.107",
-  port: 5432,
-  database: "2024v10-scr-db",
-  user: secrets.user,
-  password: secrets.password,
-});
 // const scrPool = new Pool({
 //   host: "54.205.165.107",
 //   port: 5432,
 //   database: "2024v10-scr-db",
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PWD,
+//   user: secrets.user,
+//   password: secrets.password,
 // });
+const scrPool = new Pool({
+  host: "54.205.165.107",
+  port: 5432,
+  database: "2024v10-scr-db",
+  user: process.env.PG_USER,
+  password: process.env.PG_PWD,
+});
 
 export const scrQuery = (text, params) => scrPool.query(text, params);
 
-const blnPool = new Pool({
-  host: "54.205.165.107",
-  port: 5432,
-  database: "2025v12-bln-db",
-  user: secrets.user,
-  password: secrets.password,
-});
 // const blnPool = new Pool({
 //   host: "54.205.165.107",
 //   port: 5432,
 //   database: "2025v12-bln-db",
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PWD,
+//   user: secrets.user,
+//   password: secrets.password,
 // });
+const blnPool = new Pool({
+  host: "54.205.165.107",
+  port: 5432,
+  database: "2025v12-bln-db",
+  user: process.env.PG_USER,
+  password: process.env.PG_PWD,
+});
 
 export const blnQuery = (text, params) => blnPool.query(text, params);
