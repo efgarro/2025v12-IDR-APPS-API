98e93c9d6ac0f285a670dc4543dcd86f3cf0a252
Eduardo Garro <efgarro@yahoo.com>
2026-01-12 18:40:29 -0600

    updated config files

--- src/Config/postgresPool.js
+++ src/Config/postgresPool.js
@@ -1,17 +1,4 @@
-// import { join } from "node:path";
-// import dotenv from "dotenv";
-// dotenv.config({
-//   path: join(import.meta.dirname, "../../.env"),
-// });
-
 import { secrets } from "../Utils/aws-secrets.js";
-
-// // Your code goes here
-// console.log("secret");
-// console.log(secrets);
-// console.log("User/Password");
-// console.log(secrets.user);
-// console.log(secrets.password);
 
 import pg from "pg";
 const { Pool } = pg;
@@ -23,13 +10,6 @@ const scrPool = new Pool({
   user: secrets.user,
   password: secrets.password,
 });
-// const scrPool = new Pool({
-//   host: "54.205.165.107",
-//   port: 5432,
-//   database: "2024v10-scr-db",
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PWD,
-// });
 
 export const scrQuery = (text, params) => scrPool.query(text, params);
 
@@ -40,12 +20,5 @@ const blnPool = new Pool({
   user: secrets.user,
   password: secrets.password,
 });
-// const blnPool = new Pool({
-//   host: "54.205.165.107",
-//   port: 5432,
-//   database: "2025v12-bln-db",
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PWD,
-// });
 
 export const blnQuery = (text, params) => blnPool.query(text, params);
